811edc4db7166a272dd4b0231603d7edeefb2baa
averrunci <averrunci@fievus.com>
2019-06-05 20:51:20 +0900

    Add the FromDIAttribute attribute.

---
+++ Source/Charites/FromDIAttribute.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2019 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using System;
+
+namespace Charites.Windows.Mvc
+{
+    /// <summary>
+    /// Specifies the parameter to inject from the DI container.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class FromDIAttribute : Attribute
+    {
+    }
+}
